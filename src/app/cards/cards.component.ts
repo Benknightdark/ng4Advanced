90ab9738c84dc3d7125167c6351046a06b6e7f36
Kung Ben <Kung Ben>
2017-05-23 15:13:26 +0800

    ok

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { Router, ActivatedRoute } from "@angular/router";
-=======
+
+
 import { Router,ActivatedRoute } from "@angular/router";
->>>>>>> 4736916f03892fa0e228c71a808152f7a4539b48
 
 @Component({
   selector: 'app-cards',
@@ -12,30 +10,21 @@ import { Router,ActivatedRoute } from "@angular/router";
 })
 export class CardsComponent implements OnInit {
 
-<<<<<<< HEAD
-  constructor(private router: Router, private route: ActivatedRoute) { }
-  typename: number
-  ngOnInit() {
-   // console.log(this.route.snapshot.params["type"]);
-      this.route.params.subscribe(p =>
-    { this.typename= p["types"] })
-=======
+
    constructor(private router:Router,private route:ActivatedRoute) { }
 typename:string
   ngOnInit() {
    console.log( this.route.snapshot.params["type"]);
     this.route.params.subscribe(p=>{this.typename= p["type"]})
->>>>>>> 4736916f03892fa0e228c71a808152f7a4539b48
+
 
   }
   OnPlus() {
-    this.route.params.subscribe(p =>
-    { this.typename= (<number>p["types"])+1 })
+    this.router.navigate(['/cards',parseInt(this.typename,10)+1])
 
   }
   OnMinus() {
-          this.route.params.subscribe(p =>
-          { this.typename= (<number>p["types"])-1 })
+  this.router.navigate(['/cards',parseInt(this.typename,10)-1])
 
   }
 }
