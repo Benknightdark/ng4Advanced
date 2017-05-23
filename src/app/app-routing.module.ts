211c114816addf10a51b2ee0e98ba24577e7375a
Kung Ben <Kung Ben>
2017-05-23 16:40:40 +0800

    預先載入

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
     useHash: false
 
     , enableTracing: false,
-    // preloadingStrategy: PreloadAllModules
+     preloadingStrategy: PreloadAllModules
 
   })],
   exports: [RouterModule]
