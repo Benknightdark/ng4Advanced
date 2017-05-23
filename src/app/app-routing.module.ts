1a296683c6ed167b5484a47aadbb0ebc1ae41c96
Kung Ben <Kung Ben>
2017-05-23 10:53:52 +0800

    轉向路由移至share資料夾

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule,Route } from '@angular/router';
 import { Page1Component } from "app/page1/page1.component";
 import { Page2Component } from "app/page2/page2.component";
 import { ErrorComponent } from "app/error/error.component";
+import { fallbackRoute } from "app/share/fallback-route";
 
 const routes: Routes = [
 {path: '',redirectTo:'/page1',pathMatch: 'full' },
 
   {path: 'page1', component: Page1Component},
   {path: 'page2', component: Page2Component},
-//  { path: '**', component:ErrorComponent }
-{ path: '**', redirectTo: '/', pathMatch: 'full' }
+ fallbackRoute
+//{ path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({
