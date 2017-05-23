b5d94560ff3ac289daa2786bcad72b50b6b5567b
Kung Ben <Kung Ben>
2017-05-23 09:51:43 +0800

    加入路由連結

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { Page1Component } from "app/page1/page1.component";
 import { Page2Component } from "app/page2/page2.component";
 
 const routes: Routes = [
+
   {path: 'page1', component: Page1Component},
   {path: 'page2', component: Page2Component}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  imports: [RouterModule.forRoot(routes,{useHash:true,enableTracing: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
