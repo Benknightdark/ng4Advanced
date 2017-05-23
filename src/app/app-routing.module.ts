731f139d35a01dd6c1814bdc364aec4823b28c89
Kung Ben <Kung Ben>
2017-05-23 10:35:13 +0800

    萬用和轉向路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from "app/page1/page1.component";
 import { Page2Component } from "app/page2/page2.component";
+import { ErrorComponent } from "app/error/error.component";
 
 const routes: Routes = [
+{path: '',redirectTo:'/page1',pathMatch: 'full' },
 
   {path: 'page1', component: Page1Component},
-  {path: 'page2', component: Page2Component}
+  {path: 'page2', component: Page2Component},
+//  { path: '**', component:ErrorComponent }
+{ path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
-    Page2Component
+    Page2Component,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
