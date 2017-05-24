3d764d5d54c439263b95b627e89a632deb62db65
龔彥賓 <Ben@gongyanbindeMBP.mshome.net>
2017-05-24 13:55:06 +0800

    update login code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from "app/service/login.service";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 
 @Component({
@@ -10,9 +11,12 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private LoginService: LoginService, private fb: FormBuilder) { }
+  constructor(private LoginService: LoginService, private fb: FormBuilder,private router: Router) { }
   LoginForm: FormGroup;
   ngOnInit() {
+    if (JSON.parse(localStorage.getItem("token")) === 1) {
+         this.router.navigate(['/']);
+    }
     this.LoginForm = this.fb.group({
       name: ["", Validators.compose([Validators.required])],
       password: ["", Validators.compose([Validators.required])],
