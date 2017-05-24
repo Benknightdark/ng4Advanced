762ad3c0e9f9d7664a658a4fbc0d5512daa98a3b
龔彥賓 <Ben@gongyanbindeMBP.mshome.net>
2017-05-24 14:45:02 +0800

    test

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Http ,Response, RequestOptions,Headers} from "@angular/http";
+import { Http, Response, RequestOptions, Headers } from "@angular/http";
 import { Router } from "@angular/router";
 import { LoginModel } from "app/viewmodel/loginmodel";
-import {Observable} from 'rxjs'
+import { Observable } from 'rxjs'
 import 'rxjs'
 @Injectable()
 export class LoginService {
 
   constructor(private http: Http, private router: Router) { }
-  private _url="http://192.168.137.1:88/api/token"
+  private _url = "http://192.168.137.1:88/api/token"
   JSONoptions = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json;charset=utf-8' }) });
-  Login(LoginData:LoginModel):Observable<boolean>{
-  return  this.http.post(this._url,JSON.stringify(LoginData),this.JSONoptions)
-    .map((res:Response)=>
-
-     {
+  Login(LoginData: LoginModel): Observable<boolean> {
+    return this.http.post(this._url, JSON.stringify(LoginData), this.JSONoptions)
+      .map((res: Response) => {
         let token = res.json() && res.json().token;
+        console.log(token)
         if (token) {
           console.log(token)
           localStorage.setItem('token', JSON.stringify({ username: LoginData.Username, token: token }));
@@ -26,10 +25,10 @@ export class LoginService {
 
           return false;
         }
-     }
-    )
-   // localStorage.setItem("token", "1");
-   //
+      }
+      )
+    // localStorage.setItem("token", "1");
+    //
   }
   Logout() {
     localStorage.removeItem("token");
